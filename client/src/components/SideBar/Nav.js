48fd5231e16ebd27614da80bb9acbea696e7fb79
Ayomide Daniel <adebalanced02@gmail.com>
2018-06-04 15:53:15 +0100

    uninstall hoek

--- client/src/components/SideBar/Nav.js
+++ client/src/components/SideBar/Nav.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { Collapse } from 'react-bootstrap';
 
-
 class Nav extends Component {
 
   state = {};
--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import 'sweetalert/dist/sweetalert.css';
 import Main from './pages/Main';
 import configureStore from './config/configureStore';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const store = configureStore();
 const rootElement = document.getElementById('root');
