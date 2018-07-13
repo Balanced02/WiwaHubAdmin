fff6f2d6e36eb1623f4c42fbbdcb647f3c1b46ae
Ayomide Daniel <adebalanced02@gmail.com>
2018-07-13 17:39:13 +0100

    add api to change premium product

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,21 +1,6 @@
 import regeneratorRuntime from "regenerator-runtime";
 import Products from "../models/Products";
 
-export const CreateProduct = (req, res) => {
-  const { username, product, location, title } = req.body;
-  Products.create({ username, product, location, title })
-    .then(data =>
-      res.json({
-        message: "Successfully Created"
-      })
-    )
-    .catch(err =>
-      res.status(500).json({
-        message: err.message
-      })
-    );
-};
-
 export const ChangePremium = (req, res) => {
   let id = req.params.id;
   let { premium } = req.body;
