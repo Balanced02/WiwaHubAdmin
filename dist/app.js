d468e9030703006a738f9b949de9a23be4f40671
Balanced <adebalanced02@gmail.com>
2018-09-17 14:53:02 +0100

    feadfe

--- dist/app.js
+++ dist/app.js
@@ -82,7 +82,7 @@ app.use((0, _morgan2.default)("dev", {
 app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: false }));
 app.use((0, _cors2.default)({
-  origin: "http://wiwahub.herokuapp.com",
+  origin: "http://wiwahub.herokuapp.com/",
   credentials: true
 }));
 app.use((0, _morgan2.default)("dev"));
