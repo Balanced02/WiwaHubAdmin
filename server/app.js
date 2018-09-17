9282a635bde252f104aa4f20659f654bb5c52065
Balanced <adebalanced02@gmail.com>
2018-09-17 14:48:59 +0100

    adfea

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://wiwahub.herokuapp.com",
     credentials: true
   })
 );
