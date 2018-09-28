9fd0bd7244383066e90bd6ef5f38706c1514d255
Balanced02 <adebalanced02@gmail.com>
2018-09-28 14:04:28 +0100

    redesign landing page

--- client/src/views/HomePage/index.js
+++ client/src/views/HomePage/index.js
@@ -1,140 +1,99 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption,
   Card,
-  Row, Col,
-  CardBlock,
-  Button
-} from "reactstrap";
-import WiwaHubLogo from '../../assets/img/WIWA3.jpg'
-import { Link } from 'react-router-dom'
-
-const items = [
-  {
-    src:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-    altText: "Slide 1",
-    caption: "Slide 1"
-  },
-  {
-    src:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-    altText: "Slide 2",
-    caption: "Slide 2"
-  },
-  {
-    src:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-    altText: "Slide 3",
-    caption: "Slide 3"
-  }
-];
+  Row,
+  Col,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  CardHeader,
+  CardFooter,
+  CardImg,
+} from 'reactstrap';
+import WiwaHubLogo from '../../assets/img/WIWA3.jpg';
+import { Link } from 'react-router-dom';
+import LandingPageLogo from '../../assets/img/landingPage2.jpg';
 
 class Example extends Component {
   constructor(props) {
     super(props);
-    this.state = { activeIndex: 0 };
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-    this.goToIndex = this.goToIndex.bind(this);
-    this.onExiting = this.onExiting.bind(this);
-    this.onExited = this.onExited.bind(this);
-  }
-
-  onExiting() {
-    this.animating = true;
-  }
-
-  onExited() {
-    this.animating = false;
-  }
-
-  next() {
-    if (this.animating) return;
-    const nextIndex =
-      this.state.activeIndex === items.length - 1
-        ? 0
-        : this.state.activeIndex + 1;
-    this.setState({ activeIndex: nextIndex });
-  }
-
-  previous() {
-    if (this.animating) return;
-    const nextIndex =
-      this.state.activeIndex === 0
-        ? items.length - 1
-        : this.state.activeIndex - 1;
-    this.setState({ activeIndex: nextIndex });
-  }
-
-  goToIndex(newIndex) {
-    if (this.animating) return;
-    this.setState({ activeIndex: newIndex });
+    this.state = {};
   }
 
   render() {
     const { activeIndex } = this.state;
 
-    const slides = items.map(item => {
-      return (
-        <CarouselItem
-          onExiting={this.onExiting}
-          onExited={this.onExited}
-          key={item.src}
-        >
-          <img src={item.src} alt={item.altText} width="100%" />
-          <CarouselCaption
-            captionText={item.caption}
-            captionHeader={item.caption}
-          />
-        </CarouselItem>
-      );
-    });
-
     return (
-      <Card>
-        <CardBlock>
-        <div>
-        <img src={WiwaHubLogo} width='100px' />
-        <Link to='/login' >
-        <Button color="success" outline className='pull-right' >Login</Button>
-        </Link>
-        <h1 className="display-4">WiwaHub</h1>
-        <p className="lead">One stop shop for Corp Members</p>
-        <hr className="my-2" />
-        <p className="lead">
-        <Link to='/register' >
-          <Button color="primary">Join Us!!</Button>
-        </Link>
-        </p>
-    </div>
-          <Carousel
-            activeIndex={activeIndex}
-            next={this.next}
-            previous={this.previous}
+      <Card
+        style={{
+          backgroundImage: `url(${LandingPageLogo})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          marginBottom: 0,
+        }}
+      >
+        <CardHeader
+          style={{
+            background: 'transparent',
+            padding: '10',
+            border: 'none',
+          }}
+        >
+          <CardTitle
+            style={{
+              padding: 40,
+              color: 'white',
+              fontSize: '500%',
+              textAlign: 'center',
+            }}
           >
-            <CarouselIndicators
-              items={items}
-              activeIndex={activeIndex}
-              onClickHandler={this.goToIndex}
-            />
-            {slides}
-            <CarouselControl
-              direction="prev"
-              directionText="Previous"
-              onClickHandler={this.previous}
-            />
-            <CarouselControl
-              direction="next"
-              directionText="Next"
-              onClickHandler={this.next}
-            />
-          </Carousel>
-        </CardBlock>
+            <b style={{ fontFamily: 'Lobster' }}>Wiwa</b>
+          </CardTitle>
+        </CardHeader>
+
+        <CardFooter
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            border: 'none',
+            background: 'transparent',
+            marginBottom: '70px',
+            position: 'absolute',
+            bottom: '0',
+            width: '100%',
+            height: '60px',
+          }}
+        >
+          <Link to="/register">
+            <Button
+              color="warning"
+              style={{
+                margin: 'auto',
+                width: '50%',
+                padding: '10px',
+              }}
+            >
+              SIGN UP
+            </Button>
+          </Link>
+          <Link to="/login">
+            <Button
+              color="success"
+              style={{
+                margin: 'auto',
+                width: '50%',
+                padding: '10px',
+              }}
+            >
+              LOG IN
+            </Button>
+          </Link>
+        </CardFooter>
       </Card>
     );
   }
